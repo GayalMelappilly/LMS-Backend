d6193c5e262477f96896150861461bdd904fadac
Gayal Melappilly <gayalsunil@gmail.com>
2024-06-24 01:31:23 +0530

    Free courses added successfully

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -59,3 +59,34 @@ export const editCourse = CatchAsyncError(async(req: Request, res: Response, nex
         return next(new ErrorHandler(error.message, 500))
     }
 })
+
+export const getSingleCourse = CatchAsyncError(async(req: Request, res: Response, next: NextFunction)=> {
+    try {
+       
+        const course = await CourseModel.findById(req.params.id).select('-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links')
+
+        res.status(200).json({
+            success:true,
+            course
+        })
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500))
+    }
+})
+
+
+export const getAllCourses = CatchAsyncError(async(req: Request, res: Response, next: NextFunction)=> {
+    try {
+
+        const courses = await CourseModel.find().select('-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links')
+
+        res.status(200).json({
+            success:true,
+            courses
+        })
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500))
+    }
+})
