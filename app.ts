ffbbfeb3fe5c2797e1bb411b82f7f830a7091f71
Gayal Melappilly <gayalsunil@gmail.com>
2024-06-28 02:18:06 +0530

    Created Order and added notification

--- app.ts
+++ app.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser'
 import {ErrorMiddleware} from './middleware/error'
 import userRouter from './routes/user.route'
 import courseRouter from './routes/course.route'
+import orderRouter from './routes/order.route'
 
 export const app = express()
 
@@ -15,8 +16,7 @@ app.use(cors({
     credentials: true
 }))
 
-app.use('/api/v1', userRouter)
-app.use('/api/v1', courseRouter)
+app.use('/api/v1', userRouter, courseRouter, orderRouter)
 
 app.get('/test', (req: Request,res: Response, next:NextFunction)=>{
     res.status(200).json({
