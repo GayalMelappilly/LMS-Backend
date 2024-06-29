5528995042cde497f767e7dbdbc6f0fb7a8f7691
Gayal Melappilly <gayalsunil@gmail.com>
2024-06-30 02:27:54 +0530

    Upload FAQ, Banner and Categories done

--- app.ts
+++ app.ts
@@ -8,6 +8,7 @@ import courseRouter from './routes/course.route'
 import orderRouter from './routes/order.route'
 import notificationRouter from './routes/notification.route'
 import analyticsRouter from './routes/analytics.route'
+import layoutRouter from './routes/layout.route'
 
 export const app = express()
 
@@ -18,7 +19,7 @@ app.use(cors({
     credentials: true
 }))
 
-app.use('/api/v1', userRouter, courseRouter, orderRouter, notificationRouter, analyticsRouter)
+app.use('/api/v1', userRouter, courseRouter, orderRouter, notificationRouter, analyticsRouter, layoutRouter)
 
 app.get('/test', (req: Request,res: Response, next:NextFunction)=>{
     res.status(200).json({
