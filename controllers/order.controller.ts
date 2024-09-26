bf3b358d5c92ca32d45d3ed5a6b4c1a4b7afe4f0
Gayal Melappilly <gayalsunil@gmail.com>
2024-09-27 02:01:07 +0530

    Fixed order controller issue

--- controllers/order.controller.ts
+++ controllers/order.controller.ts
@@ -3,7 +3,7 @@ import { CatchAsyncError } from '../middleware/catchAsyncError'
 import ErrorHandler from '../utils/ErrorHandler'
 import OrderModel, { IOrder } from '../models/order.model'
 import userModel from '../models/user.model'
-import CourseModel from '../models/course.model'
+import CourseModel, { ICourse } from '../models/course.model'
 import path from 'path'
 import ejs from 'ejs'
 import sendMail from '../utils/sendMail'
@@ -39,7 +39,7 @@ export const createOrder = CatchAsyncError(async (req: Request, res: Response, n
             return next(new ErrorHandler('You have already purchased this course', 400))
         }
 
-        const course = await CourseModel.findById(courseId)
+        const course:ICourse | null = await CourseModel.findById(courseId)
 
         if (!course) {
             return next(new ErrorHandler('Course not found', 404))
@@ -91,7 +91,7 @@ export const createOrder = CatchAsyncError(async (req: Request, res: Response, n
             message: `You have purchased a new course ${course.name}`
         })
 
-        course.purchased ? course.purchased += 1 : course.purchased;
+        course.purchased = course?.purchased + 1
 
         await course.save()
 
