2dfbd177e8ac2601ea3cc9371872d435407fe2d3
Gayal Melappilly <gayalsunil@gmail.com>
2024-10-15 21:58:46 +0530

    Send Mails updated

--- utils/sendMail.ts
+++ utils/sendMail.ts
@@ -10,8 +10,6 @@ interface EmailOptions{
     data: {[key:string]:any}
 }
 
-const __dirname = path.resolve();
-
 const sendMail = async (options: EmailOptions): Promise<void> => {
     const transporter: Transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
